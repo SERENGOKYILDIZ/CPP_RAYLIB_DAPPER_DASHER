e09ce21d4d4fd7bb9bdb496eb6e75084df80e869
Semi Eren Gokyildiz <gokyildizsemieren@gmail.com>
2024-11-26 20:41:52 +0300

    Made basic code

--- dasher.cpp
+++ dasher.cpp
@@ -1,6 +1,28 @@
-#include <cstdio>
+#include "raylib.h"
 
 int main()
 {
-    
+    //Window dimensions
+    const int windowWidht{500};
+    const int windowHeight{500};
+
+    //Init window
+    InitWindow(windowWidht, windowHeight, "DAPPER DASHER GAME");
+
+    //Set 60 FPS
+    SetTargetFPS(60);
+
+    //Infinite loop
+    while (!WindowShouldClose())
+    {
+        //Start drawing
+        BeginDrawing();
+
+        //Background color
+        ClearBackground(WHITE);
+
+        //End drawing
+        EndDrawing();
+
+    }
 }
