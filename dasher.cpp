73fd7500e844a0f6cccb4836e9dfa16a1807f09f
Semi Eren Gokyildiz <gokyildizsemieren@gmail.com>
2024-11-26 21:17:11 +0300

    Added velocity

--- dasher.cpp
+++ dasher.cpp
@@ -3,11 +3,19 @@
 int main()
 {
     //Window dimensions
-    const int windowWidht{500};
-    const int windowHeight{500};
+    const int windowWidht{512};
+    const int windowHeight{380};
 
     //Init window
     InitWindow(windowWidht, windowHeight, "DAPPER DASHER GAME");
+
+    // rentangle dimensions
+    const int widht{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
 
     //Set 60 FPS
     SetTargetFPS(60);
@@ -20,6 +28,16 @@ int main()
 
         //Background color
         ClearBackground(WHITE);
+
+        //Draw rentangle
+        posY += velocity;
+        DrawRectangle(windowWidht/2, posY, widht, height, BLUE);
+
+        //Moving codes
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            velocity-=10;
+        }
 
         //End drawing
         EndDrawing();
